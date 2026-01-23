47445759889f4b591f6b74536dc0b373fa2d2ac8
Narongchai Shavitranuruk <narongchai.shavi@gmail.com>
2026-01-23 07:36:57 +0700

    Add Retry pages/profile_page.py

--- tests/ui/pages/profile_page.py
+++ tests/ui/pages/profile_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from urllib3.util.retry import Retry
 
 from config.settings import BASE_URL
 import requests, time
